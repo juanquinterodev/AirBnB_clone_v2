2e0a5c2b9aa1dd85922d014cc1da708cef54a9ad
juanquinnterodev <896@holbertonschool.com>
2019-12-18 20:36:28 +0000

    update fix user

--- models/user.py
+++ models/user.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -17,3 +16,4 @@ class User(BaseModel, Base):
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)

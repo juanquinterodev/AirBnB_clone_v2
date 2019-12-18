5dd0e35af5c0568a08c92c367530bdfaa8e67af7
root <root@DESKTOP-GQSD5ER.localdomain>
2019-12-18 12:48:07 -0500

    updated basemodel and cities according to 6's specifications

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,18 @@ import models
 from datetime import datetime
 
 
+Base = declarative_base()
+
+
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -26,10 +34,10 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-        else:
+        if 'id' not in kwargs.keys():
             self.id = str(uuid.uuid4())
+        if 'created_at' not in kwargs.keys():
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """returns a string
@@ -48,7 +56,8 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        models.storage.new(self)
+        models0.storage.save()
 
     def to_dict(self):
         """creates dictionary of the class  and returns
@@ -59,4 +68,12 @@ class BaseModel:
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
+        if "_sa_instance_state" in my_dict:
+            del my_dict["_sa_instance_state"]
         return my_dict
+
+    def delete(self):
+        """
+        delete the current instance from the storage
+        """
+        models.storage.delete()

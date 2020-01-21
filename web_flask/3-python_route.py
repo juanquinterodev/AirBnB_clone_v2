f74f08ff1867c4fb5b6bf6e92461a2708699c457
juanquinterodev <896@holbertonschool.com>
2020-01-21 23:52:42 +0000

    task flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""starts a Flask web application
+"""
+from flask import Flask
+
+start = Flask(__name__)
+start.url_map.strict_slashes = False
+
+
+@start.route("/")
+def hello():
+    """display Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@start.route("/hbnb")
+def hbnb():
+    """display HBNB
+    """
+    return "HBNB"
+
+
+@start.route("/c/<text>")
+def c_text(text):
+    """display "C " with text of variable
+    """
+    return "C " + text.replace("_", " ")
+
+
+@start.route("/python/")
+@start.route("/python/<text>")
+def python_text(text="is cool"):
+    """display "Python " with the text of variable
+    """
+    return "Python " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    start.run(host="0.0.0.0", port=5000)

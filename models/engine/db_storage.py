92580697edc3b05befbfbef90b74d94ea39deaa7
juanquinnterodev <896@holbertonschool.com>
2020-01-23 03:07:02 +0000

    update pep db, state

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,7 +14,6 @@ from os import getenv
 import sqlalchemy as db
 
 
-
 class DBStorage:
     """
     database storage engine
@@ -81,3 +80,8 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         self.__session = scoped_session(sessionmaker(bind=self.__engine,
                                                      expire_on_commit=False))
+    def close(self):
+        """remove a session
+        """
+        self.__session.remove()
+
